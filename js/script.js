c6479a811330d5935655fe4c4133ca1148c4bd35
evwilkin <evan.wilkinson@gmail.com>
2016-04-02 15:14:25 -0700

    added background colors to bottom sections, changed header from vh to % of body

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
   var headerHeight = $("header").height();
   $(window).scroll(function() {
     var wScroll = $(window).scrollTop();
-    if (wScroll >= headerHeight - 200) {
+    if (wScroll >= headerHeight * .85) {
       $("a.brand-logo").fadeIn(350).removeClass("hide");
       $("nav").fadeIn(200).addClass("background");
     } else {
