44a7ed7e85ede0f8a7f152ac2f6e1d79520c855f
evwilkin <evan.wilkinson@gmail.com>
2016-04-19 22:15:43 -0700

    fixed scrolling, changed background to svg, adjusted navbar fadeout

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 $(document).ready(function(){
+
   // Collapse navbar
   $(".button-collapse").sideNav();
 
-  // Run fullpage.js
-  // $('#fullpage').fullpage({
-  //   menu: ".nav",
-  //   autoScrolling: false,
-  //   resize: true
-  // });
+  // Smooth Scroll
+  $('a').smoothScroll();
 
   // Fade in home page
   setTimeout(function() {
@@ -24,24 +21,12 @@ $(document).ready(function(){
   var headerHeight = $("header").height();
   $(window).scroll(function() {
     var wScroll = $(window).scrollTop();
-    if (wScroll >= headerHeight * .85) {
-      $("a.brand-logo").fadeIn(350).removeClass("hide");
+    if (wScroll >= headerHeight) {
+      $("a.brand-logo").fadeIn(500).removeClass("hide");
       $("nav").fadeIn(200).addClass("background");
     } else {
-      $("a.brand-logo").fadeOut(350).addClass("hide");
+      $("a.brand-logo").fadeOut(350);
       $("nav").removeClass("background");
     }
   });
 });
-
-var home = document.getElementById("home");
-var dimensions = home.getClientRects()[0];
-var pattern = Trianglify({
-    width: dimensions.width,
-    height: dimensions.height,
-    cell_size: 40, 
-    seed: 'ywn3w', 
-    x_colors: 'random'
-});
-
-home.appendChild(pattern.canvas());
